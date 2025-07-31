4024f8486d6ab749785dc2f7754f5f8e49d683e5
Ashu491784 <ayeshamadushani01@icloud.com>
2025-07-31 21:44:55 +0530

    game screen

--- src/components/MinigamesScreen.jsx
+++ src/components/MinigamesScreen.jsx
@@ -1,9 +1,55 @@
+import { motion } from "framer-motion";
+import { Link, useNavigate } from "react-router-dom";
 const MinigameScrean = () => {
-    return (
-        <div>
-            <h1>Minigames</h1>
-        </div>
-    )
-}
+  return (
+    <div className="relative w-full h-screen overflow-hidden flex items-center justify-center px-4 ">
+  
+       <video src='public\videos\bg2.mp4' 
+        autoPlay 
+         muted 
+          loop
+         playsInline
+        className='w-full h-full object-cover absolute 
+        top-0 first-letter:left-0 -z-10'>
+        </video>
+      <div className="text-center space-y-10">
+        <motion.h2
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="text-4xl md:text-5xl font-extrabold text-red-50 drop-shadow-md"
+        >
+          🎮 MINI GAME ZONE
+        </motion.h2>
 
-export default MinigameScrean
+        <motion.div
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.8, delay: 0.5 }}
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center"
+        >
+          <div className="bg-slate-700 bg-opacity-90 rounded-3xl shadow-xl p-6 w-80 text-center transform transition duration-300 hover:scale-105 hover:shadow-2xl">
+            <h3 className="text-xl font-bold text-pink-100 mb-4 font-arcade">
+              🍱 Food Maker
+            </h3>
+            <div className="flex justify-center mb-4">
+              <img
+                src="/images/foodgame.jpg"
+                alt="Food Game"
+                className="rounded-xl w-44 h-44 object-cover shadow-md border-4 border-red-500"
+              />
+            </div>
+            <button
+              onClick={() => alert("Game starting...")} 
+              className="bg-purple-300 hover:bg-blue-200 text-white hover:text-black font-bold py-2 px-6 rounded-full shadow-lg transition duration-200"
+            >
+           <Link to="/GameStart"> Play Now 🎮</Link>  
+            </button>
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default MinigameScrean;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,144 +1,170 @@
-import 'boxicons/css/boxicons.min.css';
+import "boxicons/css/boxicons.min.css";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { auth, database } from "../../firebase";
 import { ref, push } from "firebase/database";
 
-
-
 const header = () => {
+  const [theme, setTheme] = useState("dark");
+  const toggleMobileMenu = () => {
+    const MobileMenus = document.getElementById("MobileMenus"); //get mobile menu element
 
-      const [theme, setTheme] = useState('dark');
-    const toggleMobileMenu = () =>{
-        const MobileMenus = document.getElementById("MobileMenus"); //get mobile menu element
-
-        //hidden class, remove it, otherWise add it
-        if(MobileMenus.classList.contains('hidden')){
-            MobileMenus.classList.remove('hidden');
-        }else{
-            MobileMenus.classList.add('hidden')
-        }
+    //hidden class, remove it, otherWise add it
+    if (MobileMenus.classList.contains("hidden")) {
+      MobileMenus.classList.remove("hidden");
+    } else {
+      MobileMenus.classList.add("hidden");
     }
+  };
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const handleSignIn = () => {
-        navigate("/login");
-      };
-  
+    navigate("/login");
+  };
+
   return (
-    <header className="py-1 px-7 flex justify-between 
-    items-center fixed  top-0 z-50 w-full border-b-[0.3px] border-[#babaff] bg-transparent">
-        
-        {/* left section */}
-        <div className="flex lg:gap-14 gap-4 items-center">
-            <img className="md:w-16 w-12" src="public\images\logoss.png" alt="log0-img"></img>
-      
-        <div className="hidden md:flex gap-5 items-center">
-            <button className="h-8 px-6 bg-gradient-to-r from-purple-400 to-indigo-500 rounded-lg font-medium 
-            text-nowrap hover:opacity-70 transition-all duration-300">
-                PLAY NOW
-                </button>
-
-    <button className="h-8 px-6 bg-gradient-to-r from-gray-600 to-gray-400 rounded-lg font-medium 
-            text-nowrap hover:opacity-70 transition-all duration-300" onClick={handleSignIn}>Sign In
-                
-                </button>
-        </div>
-        </div>
-
-        <nav className= "hidden md:flex lg:gap-8 gap-4">
-            <a href="/Characters" className="relative py-1 text-lg hover:text-purple-300 
-            transition-colors duration-300
-             after:content-['']
-              after:absolute after:w-0 
-              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
-              after:transition-all hover:after:w-full text-nowrap">
-            <i className="bx bx-user-circle" /> Gamer 
-            </a>
-
-            <a href="/Arena" className="relative py-1 text-lg hover:text-purple-300 
-            transition-colors duration-300
-             after:content-['']
-              after:absolute after:w-0 
-              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
-              after:transition-all hover:after:w-full text-nowrap">
-            <i className="bx bx-diamond" /> Arena 
-            </a>
-            <a href="#" className="relative py-1 text-lg hover:text-purple-300 
-            transition-colors duration-300
-             after:content-['']
-              after:absolute after:w-0 
-              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
-              after:transition-all hover:after:w-full text-nowrap">
-            <i className="bx bx-chevrons-up" /> MiniGame
-            </a>
-            <a href="/Shop" className="relative py-1 text-lg hover:text-purple-300 
-            transition-colors duration-300
-             after:content-['']
-              after:absolute after:w-0 
-              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
-              after:transition-all hover:after:w-full text-nowrap">
-            <i className="bx bx-store" /> Shop
-            </a>
-        </nav>
-
-        {/* mobile munu buttons */}
-        <button onClick={toggleMobileMenu} className='text-3xl p-2 md:hidden'>
-            <i className='bx bx-menu'></i>
-        </button>
-
-        <div id='MobileMenus' className='hidden fixed top-14 right-0 left-0 bg-black p-5 md:hidden'>
-            <nav className='flex flex-col gap-4 items-center'>
-            <a href="#" className="relative py-1 text-lg hover:text-purple-300 
-            transition-colors duration-300
-             after:content-['']
-              after:absolute after:w-0 
-              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
-              after:transition-all hover:after:w-full text-nowrap">
-            <i className="bx bx-user-circle" /> Gamer
-            </a>
-
-            <a href="#" className="relative py-1 text-lg hover:text-purple-300 
-            transition-colors duration-300
-             after:content-['']
-              after:absolute after:w-0 
-              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
-              after:transition-all hover:after:w-full text-nowrap">
-            <i className="bx bx-diamond" /> Arena
-            </a>
-            <a href="#" className="relative py-1 text-lg hover:text-purple-300 
-            transition-colors duration-300
-             after:content-['']
-              after:absolute after:w-0 
-              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
-              after:transition-all hover:after:w-full text-nowrap">
-            <i className="bx bx-chevrons-up" /> Beyond
-            </a>
-            <a href="#" className="relative py-1 text-lg hover:text-purple-300 
-            transition-colors duration-300
-             after:content-['']
-              after:absolute after:w-0 
-              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
-              after:transition-all hover:after:w-full text-nowrap">
-            <i className="bx bx-store" /> Shop
-            </a>
-      
+    <header
+      className="py-1 px-7 flex justify-between 
+    items-center fixed  top-0 z-50 w-full border-b-[0.3px] border-[#babaff] bg-transparent"
+    >
      
-            </nav>
-            <div className='flex flex-col gap-3 w-full mt-4'>
-                <button className=' bg-purple-700 py-2 rounded'>
-                    Play Now 
-                    </button>
+      <div className="flex lg:gap-14 gap-4 items-center">
+        <img
+          className="md:w-16 w-12"
+          src="public\images\logoss.png"
+          alt="log0-img"
+        ></img>
 
-                    <button className='bg-gray-500 py-2  rounded'>
-                        Net Store
-                    </button>
-            </div>
+        <div className="hidden md:flex gap-5 items-center">
+          <button
+            className="h-8 px-6 bg-gradient-to-r from-purple-400 to-indigo-500 rounded-lg font-medium 
+            text-nowrap hover:opacity-70 transition-all duration-300"
+          >
+            PLAY NOW
+          </button>
+
+          <button
+            className="h-8 px-6 bg-gradient-to-r from-gray-600 to-gray-400 rounded-lg font-medium 
+            text-nowrap hover:opacity-70 transition-all duration-300"
+            onClick={handleSignIn}
+          >
+            Sign In
+          </button>
         </div>
+      </div>
+
+      <nav className="hidden md:flex lg:gap-8 gap-4">
+        <a
+          href="/Characters"
+          className="relative py-1 text-lg hover:text-purple-300 
+            transition-colors duration-300
+             after:content-['']
+              after:absolute after:w-0 
+              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
+              after:transition-all hover:after:w-full text-nowrap"
+        >
+          <i className="bx bx-user-circle" /> Gamer
+        </a>
+
+        <a
+          href="/Arena"
+          className="relative py-1 text-lg hover:text-purple-300 
+            transition-colors duration-300
+             after:content-['']
+              after:absolute after:w-0 
+              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
+              after:transition-all hover:after:w-full text-nowrap"
+        >
+          <i className="bx bx-diamond" /> Arena
+        </a>
+        <a
+          href="/Minigames"
+          className="relative py-1 text-lg hover:text-purple-300 
+            transition-colors duration-300 after:content-['']
+              after:absolute after:w-0 
+              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
+              after:transition-all hover:after:w-full text-nowrap"
+        >
+          <i className="bx bx-chevrons-up" /> MiniGame
+        </a>
+        <a
+          href="/Shop"
+          className="relative py-1 text-lg hover:text-purple-300 
+            transition-colors duration-300
+             after:content-['']
+              after:absolute after:w-0 
+              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
+              after:transition-all hover:after:w-full text-nowrap"
+        >
+          <i className="bx bx-store" /> Shop
+        </a>
+      </nav>
+
+      {/* mobile munu buttons */}
+      <button onClick={toggleMobileMenu} className="text-3xl p-2 md:hidden">
+        <i className="bx bx-menu"></i>
+      </button>
+
+      <div
+        id="MobileMenus"
+        className="hidden fixed top-14 right-0 left-0 bg-black p-5 md:hidden"
+      >
+        <nav className="flex flex-col gap-4 items-center">
+          <a
+            href="#"
+            className="relative py-1 text-lg hover:text-purple-300 
+            transition-colors duration-300
+             after:content-['']
+              after:absolute after:w-0 
+              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
+              after:transition-all hover:after:w-full text-nowrap"
+          >
+            <i className="bx bx-user-circle" /> Gamer
+          </a>
+
+          <a
+            href="#"
+            className="relative py-1 text-lg hover:text-purple-300 
+            transition-colors duration-300
+             after:content-['']
+              after:absolute after:w-0 
+              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
+              after:transition-all hover:after:w-full text-nowrap"
+          >
+            <i className="bx bx-diamond" /> Arena
+          </a>
+          <a
+            href="#"
+            className="relative py-1 text-lg hover:text-purple-300 
+            transition-colors duration-300
+             after:content-['']
+              after:absolute after:w-0 
+              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
+              after:transition-all hover:after:w-full text-nowrap"
+          >
+            <i className="bx bx-chevrons-up" /> Beyond
+          </a>
+          <a
+            href="#"
+            className="relative py-1 text-lg hover:text-purple-300 
+            transition-colors duration-300
+             after:content-['']
+              after:absolute after:w-0 
+              after:h-0.5  after:bg-purple-400 after:left-0 after:bottom-0 
+              after:transition-all hover:after:w-full text-nowrap"
+          >
+            <i className="bx bx-store" /> Shop
+          </a>
+        </nav>
+        <div className="flex flex-col gap-3 w-full mt-4">
+          <button className=" bg-purple-700 py-2 rounded">Play Now</button>
+
+          <button className="bg-gray-500 py-2  rounded">Net Store</button>
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-
-export default header
+export default header;
