41aa75b94da028fcadadb4c689dec7203ff64039
Ashu491784 <ayeshamadushani01@icloud.com>
2025-09-18 12:30:57 +0530

    admn dashboard add

--- src/User/AddUser.jsx
+++ src/User/AddUser.jsx
@@ -65,7 +65,7 @@ const AddUsers = () => {
 
   return (
     <div className="mt-6">
-        {/* update form */}
+      
       {editingUser && (
         <div className="mb-6 p-4 bg-gray-100 dark:bg-gray-800 rounded-lg shadow">
           <h2 className="text-lg font-bold mb-4 text-gray-700 dark:text-gray-200">
